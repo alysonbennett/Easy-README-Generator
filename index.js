a97941a91ec663579284a57012aa72a159538b63
alysonbennett <alysonbennett@gmai.com>
2020-11-08 19:04:04 -0500

    added questions array and variables

--- index.js
+++ index.js
@@ -1,6 +1,66 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+const generate = require("./utils/generateMarkdown");
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your GitHub username?"
 
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?"
+
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project"
+
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?"
+
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the user need to know about using the repo?"
+
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Choose a license",
+        default: "MIT",
+        choices: [
+            "MIT",
+            "APACHE 2.0",
+            "GPL 3.0",
+            "BSD 3",
+            "None"
+        ]
+
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?"
+
+    },
 ];
 
 // function to write README file
